02b537f1c8cea10e5870e85ae9cee6e6f4c6fd90
Daniel S. Ramos <danikzero@hotmail.com>
2025-08-18 16:10:35 -0300

    :sparkles: feat: dados para popular sites

---
+++ assets/js/dados_brands.js
@@ -0,0 +1,11 @@
+const brands = [
+  "/assets/img/logos-svg/Acer_2011.svg",
+  "/assets/img/logos-svg/Amazon_Games_2025.svg",
+  "/assets/img/logos-svg/AMD_Logo.svg",
+  "/assets/img/logos-svg/Bosch-Logo.svg",
+  "/assets/img/logos-svg/Canon_logo.svg",
+  "/assets/img/logos-svg/Casio_logo.svg",
+  "/assets/img/logos-svg/Epson_logo.svg",
+  "/assets/img/logos-svg/Microsoft_logo_(1987).svg",
+  "/assets/img/logos-svg/Xerox_logo_(1968-2008).svg",
+];
---
+++ assets/js/dados_destaque.js
@@ -0,0 +1,194 @@
+const destaques = [
+  {
+    id: 1,
+    nome: "Hidraulica",
+    imagem: "/assets/img/products/Registro-de-pressao-cromado-300x300.png",
+    link: "#ver-produto",
+    itens: [
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: "Hidraulica",
+    imagem: "/assets/img/products/Registro-de-pressao-cromado-300x300.png",
+    link: "#ver-produto",
+    itens: [
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+    ],
+  },
+  {
+    id: 3,
+    nome: "Hidraulica",
+    imagem: "/assets/img/products/Registro-de-pressao-cromado-300x300.png",
+    link: "#ver-produto",
+    itens: [
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+    ],
+  },
+  {
+    id: 4,
+    nome: "Hidraulica",
+    imagem: "/assets/img/products/Registro-de-pressao-cromado-300x300.png",
+    link: "#ver-produto",
+    itens: [
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+    ],
+  },
+  {
+    id: 5,
+    nome: "Hidraulica",
+    imagem: "/assets/img/products/Registro-de-pressao-cromado-300x300.png",
+    link: "#ver-produto",
+    itens: [
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+    ],
+  },
+  {
+    id: 6,
+    nome: "Hidraulica",
+    imagem: "/assets/img/products/Registro-de-pressao-cromado-300x300.png",
+    link: "#ver-produto",
+    itens: [
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+    ],
+  },
+  {
+    id: 7,
+    nome: "Hidraulica",
+    imagem: "/assets/img/products/Registro-de-pressao-cromado-300x300.png",
+    link: "#ver-produto",
+    itens: [
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+    ],
+  },
+  {
+    id: 8,
+    nome: "Hidraulica",
+    imagem: "/assets/img/products/Registro-de-pressao-cromado-300x300.png",
+    link: "#ver-produto",
+    itens: [
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+      {
+        nome: "Registro de pressão",
+        link: "/produtos/hidraulica/registro",
+      },
+    ],
+  },
+];
